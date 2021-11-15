5aa7bfe9d2b4fccd626d944006468b4dd88612ef
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-11-15 09:57:13 -0700

    removed auth for local mongo development, user creation boilerplate

--- ticketing-app/auth/src/routes/sign-in.ts
+++ ticketing-app/auth/src/routes/sign-in.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { ApiValidationError } from "../errors/api-validation-error";
 import { getLogger } from "../logger/create-client";
+import { User } from "../database/models/user";
 
 const router = express.Router();
 const logger = getLogger();
@@ -16,12 +17,21 @@ const validator = [
     .withMessage("password must be between 4 and 20 characters")
 ];
 
-router.post("/api/users/signin", validator, (req: Request, res: Response): void => {
+router.post("/api/users/signin", validator, async (req: Request, res: Response) => {
   logger.info("Logging user in!");
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     throw new ApiValidationError(errors.array());
+  }
+
+  const existingUser = await User.findOne({ email: req.body.email });
+
+  const { email, password } = req.body;
+
+  if (existingUser === null) {
+    const newUser = User.build({ email, password });
+    await User.create(newUser);
   }
 
   logger.info("creating a user!");
